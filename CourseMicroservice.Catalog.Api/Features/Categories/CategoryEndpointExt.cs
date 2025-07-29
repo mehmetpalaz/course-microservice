25467cdfb8a583786579ea78f691917b77dbc328
Mehmet Palaz <m.palaz@papara.com>
2025-07-29 12:54:21 +0300

    feat(catalog): implement create category endpoint with MediatR

---
+++ CourseMicroservice.Catalog.Api/Features/Categories/CategoryEndpointExt.cs
@@ -0,0 +1,14 @@
+﻿using CourseMicroservice.Catalog.Api.Features.Categories.Create;
+
+namespace CourseMicroservice.Catalog.Api.Features.Categories
+{
+    public static class CategoryEndpointExt
+    {
+        public static WebApplication AddCategoryEnpointExt(this WebApplication app)
+        {
+            app.MapGroup("/api/categories").CreateCategoryGroupItem();
+
+            return app;
+        }
+    }
+}
