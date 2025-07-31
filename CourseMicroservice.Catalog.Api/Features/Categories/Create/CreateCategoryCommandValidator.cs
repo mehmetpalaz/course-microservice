10d7872c3d0b10f7b51ad64ec9aea26484bf2c6f
Mehmet Palaz <m.palaz@papara.com>
2025-07-31 23:36:57 +0300

    feat(catalog): add validator for create category with FluentValidator library

---
+++ CourseMicroservice.Catalog.Api/Features/Categories/Create/CreateCategoryCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace CourseMicroservice.Catalog.Api.Features.Categories.Create
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .Length(4, 100).WithMessage("Name must be between 4 and 100 characters.");
+        }
+    }
+}
